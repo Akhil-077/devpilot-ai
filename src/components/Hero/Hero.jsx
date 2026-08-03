60c7b90ac33e0f8f83fe7edf62f8ac9663924ecb
Akhil-077 <azmeeraakhil123@gmail.com>
2026-08-03 23:59:29 +0530

    Initial React project structure

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+function Hero() {
+  return (
+    <section>
+      <h2>Build AI Applications Like Professionals</h2>
+      <p>
+        Learn, Build, Deploy and Grow with AI.
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
